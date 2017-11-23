175ae1df81f762cc09de32d78fef5ddc817bb6b8
christophe ridarch <christophe.ridarch@gmail.com>
2017-11-23 18:28:23 +0100

    Add Dose feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :new, :create]
-
-  # do
-  #   resources :doses, only [:new, :create]
-  # end
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
 end
+
