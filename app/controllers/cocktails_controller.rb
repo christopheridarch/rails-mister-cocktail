5348d3712df6d8ac06556440ec1a5ae4604c6a96
christophe ridarch <christophe.ridarch@gmail.com>
2017-11-23 17:27:40 +0100

    Create Cocktail Feature

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,9 +10,16 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.create
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
