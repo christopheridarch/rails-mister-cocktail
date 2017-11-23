a23022e84e78af86c75768184d811a32463b841e
christophe ridarch <christophe.ridarch@gmail.com>
2017-11-23 16:27:07 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'JSON'
+
+Ingredient.delete_all
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+JSON.parse(open(url).read)["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
